8622911bd152f2f1777d027c69c1cd6c135101a7
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-10-17 13:41:11 +0200

    cleaner

--- fashionnets/callbacks/delete_checkpoints.py
+++ fashionnets/callbacks/delete_checkpoints.py
@@ -21,49 +21,113 @@ class DeleteOldModel(keras.callbacks.Callback):
         self.keep_n = keep_n
         self.only_weights = save_weights_only
 
+
     def on_epoch_end(self, epoch, logs=None):
         fname_checkpoints = self.list_checkpoints()
 
         if self.keep_n >= len(fname_checkpoints):
             return
 
-        if not self.only_weights:
-            clean_name = lambda n: int(n.replace(self.name, "").replace(self.ext, "").split("_ep")[-1])
-        else:
-            clean_name = lambda x: x.split(".")[0].split("_ep")[-1]
+        best_cps, train_cp = self.sorted_checkpoints(fname_checkpoints)
+        best_cps, train_cp = best_cps[:-self.keep_n], train_cp[:-self.keep_n]
 
-        get_ep = lambda d: (clean_name(d[0]), d[1])
-        fname_checkpoints = list(map(get_ep, fname_checkpoints))
-        sorted_cps = sorted(fname_checkpoints, key=lambda tup: tup[0])
+        for files_to_del in [best_cps, train_cp]:
+            for ep, paths in files_to_del:
+                for path in paths:
+                    path = Path(path)
+                    print(f"Removing CP (EP={ep}): ./{path.name}")
+                    path.unlink()
 
-        delete_cps = list(sorted_cps)
+    def sorted_checkpoints(self, checkpoints):
+        best_cps, train_cp = defaultdict(lambda: []), defaultdict(lambda: [])
 
-        if not self.only_weights:
-            for ep, path in delete_cps[:-self.keep_n]:
-                print(f"Removing CP (EP={ep}): ./{path.name}")
-                path.unlink()
-        else:
-            paths_by_ep = defaultdict(lambda: [])
-            for ep, path in delete_cps:
-                paths_by_ep[ep].append(path)
+        for ep, paths in list(checkpoints.items()):
+            for path in paths:
+                if "_best_" in path:
+                    best_cps[ep].append(path)
+                else:
+                    train_cp[ep].append(path)
 
-            paths_by_ep = collections.OrderedDict(sorted(paths_by_ep.items()))
-            to_delete = list(paths_by_ep.items())[:-self.keep_n]
-            for ep, paths in to_delete:
-                for p in paths:
-                    print(f"Removing CP (EP={ep}): ./{p.name}")
-                    p.unlink()
+        best_cps, train_cp = best_cps.items(), train_cp.items()
 
-    def list_checkpoints(self, keep_best=True):
-        if not self.only_weights:
-            is_checkpoint = lambda x: x[-3:] == self.ext
-        else:
-            is_checkpoint = lambda x: "_ep" in x and (x.endswith(".index") or ".data-" in x)
-        is_not_best = lambda x: not "_best_" in x
+        return list(best_cps), list(train_cp)
 
-        checkpoints = filter(is_checkpoint, os.listdir(self.checkpoint_path))
-        if keep_best:
-            checkpoints = filter(is_not_best, checkpoints)
-        f_names = list(checkpoints)
-        fullpaths = list(map(lambda p: Path(self.checkpoint_path + f"/{p}"), f_names))
-        return list(zip(f_names, fullpaths))
+
+    def list_checkpoints(self):
+        cp_exts = ["ckpt", "index", "data", "tf", "h5"]
+        unpack_path = lambda p: (os.path.join(self.checkpoint_path, p), *os.path.splitext(p))
+        remove_ext_col = lambda d: (d[0], d[1])
+        ep_from_name = lambda n: n.split(".ckpt")[0].split("-")[1]  ##cp-0001.ckpt -> 0001
+
+        fname_to_ep = lambda d: (d[0], ep_from_name(d[1].replace(self.name, "")))
+
+        def is_cp(d):
+            ext = d[2]
+            for cp_ext in cp_exts:
+                if cp_ext in ext:
+                    return True
+            return False
+
+        apath_name_ext = map(unpack_path, os.listdir(self.checkpoint_path))
+        apath_name_ext = filter(is_cp, apath_name_ext)
+        apath_name_ext = map(remove_ext_col, apath_name_ext)
+        apath_name_ext = map(fname_to_ep, apath_name_ext)
+
+        paths_by_ep = defaultdict(lambda: [])
+
+        for apath, epoch in apath_name_ext:
+            paths_by_ep[int(epoch)].append(apath)
+
+        return collections.OrderedDict(sorted(paths_by_ep.items()))
+
+if __name__ == "__main__":
+    dom = DeleteOldModel(checkpoint_path="F:\workspace\FashNets\simplecnn_none", keep_n=1, name="simplecnn_none")
+    dom.on_epoch_end(None, None)
+
+
+##    def on_epoch_end(self, epoch, logs=None):
+##        fname_checkpoints = self.list_checkpoints()
+##
+##        if self.keep_n >= len(fname_checkpoints):
+##            return
+
+##        if not self.only_weights:
+##            clean_name = lambda n: int(n.replace(self.name, "").replace(self.ext, "").split("_ep")[-1])
+##        else:
+##            clean_name = lambda x: x.split(".")[0].split("_ep")[-1]
+
+##        get_ep = lambda d: (clean_name(d[0]), d[1])
+##        fname_checkpoints = list(map(get_ep, fname_checkpoints))
+##        sorted_cps = sorted(fname_checkpoints, key=lambda tup: tup[0])
+
+##        delete_cps = list(sorted_cps)
+
+##        if not self.only_weights:
+##            for ep, path in delete_cps[:-self.keep_n]:
+##                print(f"Removing CP (EP={ep}): ./{path.name}")
+##                path.unlink()
+##        else:
+##            paths_by_ep = defaultdict(lambda: [])
+##            for ep, path in delete_cps:
+##                paths_by_ep[ep].append(path)
+
+##            paths_by_ep = collections.OrderedDict(sorted(paths_by_ep.items()))
+##            to_delete = list(paths_by_ep.items())[:-self.keep_n]
+##            for ep, paths in to_delete:
+##                for p in paths:
+##                    print(f"Removing CP (EP={ep}): ./{p.name}")
+##                    p.unlink()
+
+#    def list_checkpoints(self, keep_best=True):
+#        if not self.only_weights:
+#            is_checkpoint = lambda x: x[-3:] == self.ext
+#        else:
+#            is_checkpoint = lambda x: "_ep" in x and (x.endswith(".index") or ".data-" in x)
+#        is_not_best = lambda x: not "_best_" in x
+
+#        checkpoints = filter(is_checkpoint, os.listdir(self.checkpoint_path))
+#        if keep_best:
+#            checkpoints = filter(is_not_best, checkpoints)
+#        f_names = list(checkpoints)
+#        fullpaths = list(map(lambda p: Path(self.checkpoint_path + f"/{p}"), f_names))
+#        return list(zip(f_names, fullpaths))
