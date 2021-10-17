1662cdbc91327dd971cbca6c0cf45e84a427e8a3
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-10-17 12:44:33 +0200

    cp path

--- fashionnets/callbacks/callbacks.py
+++ fashionnets/callbacks/callbacks.py
@@ -18,8 +18,12 @@ def callbacks(checkpoint_path, name, monitor='val_loss', save_format=None, save_
     ]
 
 def model_checkpoint(checkpoint_path, name, monitor='val_accuracy', save_weights_only=False):
-    model_cp_path = Path(checkpoint_path, f"{name}_best_only" + "_{epoch}") # .h5
-    model_cp_latest_path = Path(checkpoint_path, name+"_latest_ep{epoch}") #.h5
+    model_cp_path = Path(checkpoint_path, f"{name}_best_") # .h5
+    model_cp_latest_path = Path(checkpoint_path, name+"_") #.h5
+
+    model_cp_path = str(model_cp_path.resolve()) + "cp-{epoch:04d}.ckpt"
+    model_cp_latest_path = str(model_cp_latest_path.resolve()) + "cp-{epoch:04d}.ckpt"
+
     history_cp_path = Path(checkpoint_path, "history.csv")
 
     model_cp_path.parent.mkdir(parents=True, exist_ok=True)
