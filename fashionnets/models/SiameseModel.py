f390dd5547c68dc9f6d8a9d1909d94f263f3f85e
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-10-18 11:55:42 +0200

    verbose prints

--- fashionnets/models/SiameseModel.py
+++ fashionnets/models/SiameseModel.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from fashiondatasets.utils.logger.defaultLogger import defaultLogger
 from tensorflow.keras import Model
 from tensorflow.keras import metrics
 from tensorflow.keras import optimizers
@@ -13,27 +14,31 @@ class SiameseModel(Model):
         super(SiameseModel, self).__init__()
         self.siamese_network = siamese_network
         self.loss_tracker = metrics.Mean(name="loss")
+        self.logger = defaultLogger(name="Siamese_Modell")
 
     def call(self, inputs):
         return self.siamese_network(inputs)
 
     def train_step(self, data):
+        self.logger.debug("Train_Step: Calc Loss")
         with tf.GradientTape() as tape:
             loss = self.siamese_network(data)
-
+        self.logger.debug("Train_Step: Tape.Gradient")
         gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
-
+        self.logger.debug("Train_Step: Gradient.Apply")
         self.optimizer.apply_gradients(
             zip(gradients, self.siamese_network.trainable_weights)
         )
-
+        self.logger.debug("Train_Step: Update Loss")
         self.loss_tracker.update_state(loss)
         return {"loss": self.loss_tracker.result()}
 
     def test_step(self, data):
+        self.logger.debug("Test_Step: Calc Val_Loss")
         loss = self.siamese_network(data)
 
         self.loss_tracker.update_state(loss)
+        self.logger.debug("Test_Step: Update Val_Loss")
         return {"loss": self.loss_tracker.result()}
 
     def fake_predict(self, input_shape, is_triplet):
