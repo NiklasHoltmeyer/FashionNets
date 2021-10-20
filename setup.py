85e288f112842bce63ac700bc55c62a696f589ba
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-10-20 18:24:29 +0200

    fixing imports

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-# with open('LICENSE') as f:
-#   license = f.read()
-#
-# with open('README.rst') as f:
-#   readme = f.read()
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name='fashion_nets',
     version='0.1.0',
     description='',
-    # long_description=readme,
+    long_description=readme,
     author='Niklas Holtmeyer',
     url='https://github.com/NiklasHoltmeyer/FashionNets',
-    # license=license,
+    license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=[] #required,
+    install_requires=required
 )
