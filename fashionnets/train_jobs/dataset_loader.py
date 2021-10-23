39abcb6d2defc9b82a766b8ba20b5480c7a8e24f
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-10-23 09:55:58 +0200

    preprocess ds

--- fashionnets/train_jobs/dataset_loader.py
+++ fashionnets/train_jobs/dataset_loader.py
@@ -1,5 +1,9 @@
-from pathlib import Path
+import tensorflow as tf
+from fashiondatasets.deepfashion2.DeepFashionQuadruplets import DeepFashionQuadruplets
+from fashiondatasets.own.Quadruplets import Quadruplets
+from fashiondatasets.own.helper.mappings import preprocess_image
 
+from fashionnets.train_jobs.job_loader import _load_dataset_base_path
 from fashionnets.util.io import all_paths_exist
 
 
@@ -24,3 +28,121 @@ def deep_fashion_loader_info(variation):
             "check_existence": lambda: all_paths_exist(["./deep_fashion_256", "./deep_fashion"])
         }
     }
+
+def load_dataset(**settings):
+    ds_name = settings["dataset"]["name"]
+    if ds_name== "own":
+        return load_own_dataset(**settings)
+    if ds_name == "deep_fashion_256":
+        return load_deepfashion(**settings)
+    raise Exception(f'Unknown Dataset {ds_name}')
+
+
+def _fill_ds_settings(**settings):
+    return {
+            "map_full_paths": settings.get("map_full_paths", True),
+            "validate_paths": settings.get("validate_paths", False),
+            "format": settings.get("format", "triplet"),  # "quadruplet", # triplet
+            "nrows": settings.get("nrows", None),
+            "target_shape": settings.get("target_shape", (144, 144)),
+            "batch_size": settings.get("batch_size", 32),
+            "buffer_size": settings.get("buffer_size", 1024),
+            "train_split": settings.get("train_split", 0.8),
+            "preprocess_input": settings.get("preprocess_input", None)
+        }
+
+def _print_ds_settings(verbose, **ds_settings):
+    if verbose:
+        header_str = "*" * 24 + " Settings " + "*" * 24
+        print(header_str)
+        print("{")
+        for k, v in ds_settings.items():
+            tab = " " * 8
+            k_str = f"{tab}'{k}':"
+            v_str = f"'{v}'"
+            padding_len = len(header_str) - len(k_str) - len(v_str) - len(tab)
+            padding = max(padding_len, 0)
+            pad_str = " " * padding
+            print(f"{k_str}{pad_str}{v_str}")
+        print("}")
+        print("*" * len(header_str))
+
+def load_deepfashion(**settings):
+    ds_settings = _fill_ds_settings(**settings)
+    _print_ds_settings(settings.get("verbose", False), **ds_settings)
+    base_path = _load_dataset_base_path(**settings)
+    datasets = DeepFashionQuadruplets(base_path=base_path, split_suffix="_256", format=settings["format"])\
+        .load_as_datasets(validate_paths=False)
+    train_ds_info, val_ds_info = datasets["train"], datasets["validation"]
+
+    train_ds, val_ds = train_ds_info["dataset"], val_ds_info["dataset"]
+
+    settings["_dataset"] = settings.pop("dataset") #<- otherwise kwargs conflict 2x ds
+    train_ds, val_ds = prepare_ds(train_ds, **settings), prepare_ds(val_ds, **settings)
+
+    n_train, n_val = train_ds_info["n_items"], val_ds_info["n_items"]
+
+    return{
+        "train": train_ds,
+        "val": val_ds,
+        "shape": ds_settings.get("target_shape"),
+        "n_items": {
+            "total": n_val+n_train,
+            "validation": n_val,
+            "train": n_train
+        }
+    }
+
+def load_own_dataset(**settings):
+    ds_settings = _fill_ds_settings(**settings)
+    _print_ds_settings(settings.get("verbose", False), ds_settings)
+
+
+    base_path = _load_dataset_base_path(**settings)
+    train_dataset, val_dataset, n_train, n_val = _load_own_dataset(**{"base_path":base_path, **ds_settings})
+    return {
+        "train": train_dataset,
+        "val": val_dataset,
+        "shape": ds_settings.get("target_shape"),
+        "n_items": {
+            "total": n_val+n_train,
+            "validation": n_val,
+            "train": n_train
+        }
+    }
+
+
+def _load_own_dataset(base_path, batch_size, buffer_size, train_split, format, **settings):
+    split = train_split
+    settings["format"] = format
+
+    quad = Quadruplets(base_path, **settings)
+
+    dataset = quad.load_as_dataset()
+    dataset = dataset.shuffle(buffer_size)
+
+    n_total_items = len(quad)
+    n_train_items = round(split * n_total_items) #  // batch_size
+    n_val_items = n_total_items - n_train_items
+
+    train_dataset = dataset.take(n_train_items)
+    train_dataset = prepare_ds(train_dataset)
+
+    val_dataset = dataset.skip(n_train_items)
+    val_dataset = prepare_ds(val_dataset)
+
+    return train_dataset, val_dataset, n_train_items, n_val_items
+
+def prepare_ds(dataset, batch_size, **settings):
+    return dataset.map(_load_image_preprocessor(**settings)) \
+        .batch(batch_size, drop_remainder=False) \
+        .prefetch(tf.data.AUTOTUNE)
+
+def _load_image_preprocessor(format, target_shape, preprocess_input=None, **kwargs):
+    is_triplet = "triplet" in format
+    prep_image = preprocess_image(target_shape, preprocess_img=preprocess_input)
+
+    if is_triplet:
+        return lambda a, p, n: (prep_image(a), prep_image(p), prep_image(n))
+    else:
+        return lambda a, p, n1, n2: (prep_image(a), prep_image(p), prep_image(n1), prep_image(n2))
---
+++ fashionnets/train_jobs/model_loader.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+from fashiondatasets.utils.logger.defaultLogger import defaultLogger
+from tensorflow import keras
+
+from fashionnets.callbacks.callbacks import callbacks
+from fashionnets.models.SiameseModel import SiameseModel
+from fashionnets.networks.SiameseNetwork import SiameseNetwork
+from fashionnets.util.csv import HistoryCSVHelper
+
+
+def get_checkpoint(train_job, logger):
+    cp_path, name = train_job["path"]["checkpoint"], train_job["run"]["name"]
+
+    last_epoch = HistoryCSVHelper.last_epoch_from_train_job(train_job)
+    init_epoch = last_epoch + 1
+
+    if init_epoch > 0:
+        checkpoint = str(Path(cp_path, name)) + f"_cp-{init_epoch:04d}.ckpt"
+
+        logger.debug("Resume Training:")
+        logger.debug(f" - Initial Epoch: " + init_epoch)
+        logger.debug(f" - Checkpoint:    " + checkpoint)
+
+        return init_epoch, checkpoint
+    else:
+        logger.debug(f"No Checkpoints found in: {cp_path}")
+    return init_epoch, None
+
+
+def load_siamese_model(train_job, input_shape, keep_n=1, optimizer=None, verbose=False, result_uploader=None):
+    logger = defaultLogger("Load_Siamese_Model")
+    logger.disabled = not verbose
+
+    if not optimizer:
+        optimizer = keras.optimizers.Adam(1e-3)
+        logger.debug("Default Optimizer: Adam(LR 1e-3)")
+    else:
+        logger.debug("Using non Default Optimizer")
+
+    parm_list = ["back_bone", "triplets", "input_shape", "alpha", "beta", "verbose", "channels"]
+    kwargs = {k: train_job[k] for k in parm_list if train_job.get(k, None)}
+    kwargs["triplets"] = kwargs.get("triplets", train_job["triplets"])
+    logger.debug(f"Loading Siamese Network: {kwargs}")
+    siamese_network = SiameseNetwork.build(**kwargs)
+
+    siamese_model = SiameseModel(siamese_network)
+    siamese_model.compile(optimizer=optimizer)
+    siamese_model.fake_predict(input_shape, train_job["is_triplet"])
+
+    cp_path, run_name = train_job["path"]["checkpoint"], train_job["run"]["name"]
+
+    logger.debug(f"Checkpoint: {cp_path}")
+    logger.debug(f"Run Name: {run_name}")
+
+    _callbacks = callbacks(cp_path, run_name, monitor='val_loss', keep_n=keep_n,
+                           verbose=verbose, result_uploader=result_uploader)
+
+    logger.debug(f"Callbacks: {_callbacks}")
+
+    init_epoch, _checkpoint = get_checkpoint(train_job, logger)
+    if _checkpoint:
+        logger.debug("Loading Weights!")
+        siamese_model.load_weights(_checkpoint)
+
+    return siamese_model, init_epoch, _callbacks
