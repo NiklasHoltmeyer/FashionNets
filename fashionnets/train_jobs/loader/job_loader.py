d67909ca2a6112302514ec42e96802dedf42cf87
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-10-23 10:33:29 +0200

    preprocess ds

--- fashionnets/train_jobs/loader/job_loader.py
+++ fashionnets/train_jobs/loader/job_loader.py
@@ -3,7 +3,7 @@ from fashionnets.train_jobs.notebook_environments.consts import notebooks
 
 def _load_checkpoint_path(run_name, **settings):
     notebook = settings["notebook"]
-    assert notebook in notebooks[notebook].keys()
+    assert notebook in notebooks.keys()
 
     checkpoint_path = notebooks[notebook]["paths"]["checkpoint"] + run_name
 
@@ -12,7 +12,7 @@ def _load_checkpoint_path(run_name, **settings):
 def _load_dataset_base_path(**settings):
     #F:\workspace\datasets\own_256
     notebook = settings["notebook"]
-    assert notebook in notebooks[notebook].keys()
+    assert notebook in notebooks.keys()
 
     return notebooks[notebook]["paths"]["dataset_base_path"] + settings["dataset"]["name"]
 
